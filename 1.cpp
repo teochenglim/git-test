e5b8c69907a05f1852a2d22b27e16e27922f802e
Teo Cheng Lim <teochenglim@gmail.com>
2014-12-28 14:50:35 +0800

    test 3

--- 1.cpp
+++ 1.cpp
@@ -1,7 +1,7 @@
-#include <iostream.h>
-
-main()
-{
-    cout << "Hello World!";
-    return 0;
+#include <iostream>
+using namespace std;
+ 
+int main() {
+   cout << "Hello, world!" << endl;
+   return 0;
 }
